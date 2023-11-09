2d62e860a89b2ee0425d70d24eb715ca9132dfee
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2023-11-09 06:27:30 +0100

    theme done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,28 @@
 const Navbar = () => {
-
   const themeToggleFn = () => {
-    const nav = document.querySelector('.navbar-class')
-    const themeButton = document.querySelector('.theme')
-    const app = document.querySelector('.app')
-    const searchInput = document.querySelector('.search-input')
-    const filterButton = document.querySelector('.filter-button')
-    const hiddenFilterContainer = document.querySelector('.hidden-filter-container')
-    const singleCountryCard = document.querySelector('.single-country-card')
-    const logo = document.querySelector('.logo')
+    const nav = document.querySelector(".navbar-class");
+    const themeButton = document.querySelector(".theme");
+    const app = document.querySelector(".app");
+    const searchInput = document.querySelector(".search-input");
+    const filterButton = document.querySelector(".filter-button");
+    const hiddenFilterContainer = document.querySelector(
+      ".hidden-filter-container"
+    );
+    const logo = document.querySelector(".logo");
+    const signleCountryCards = document.querySelectorAll(".single-country-card");
 
-    nav.classList.toggle('dark')
-    themeButton.classList.toggle('dark')
-    app.classList.toggle('dark')
-    searchInput.classList.toggle('dark')
-    filterButton.classList.toggle('dark')
-    hiddenFilterContainer.classList.toggle('dark')
-    singleCountryCard.classList.toggle('dark')
-    logo.classList.toggle('dark')
+    nav.classList.toggle("dark");
+    themeButton.classList.toggle("dark");
+    app.classList.toggle("dark");
+    searchInput.classList.toggle("dark");
+    filterButton.classList.toggle("dark");
+    hiddenFilterContainer.classList.toggle("dark");
+    logo.classList.toggle("dark");
+
+    signleCountryCards.forEach((card) => {
+      card.classList.toggle("dark");
+    });
+
   };
 
   return (
